2b1f7cd6d21933078b4a56444e486a2c77b55a15
Michaela Budde <michaelabudde@lilas-mbp.mynetworksettings.com>
2024-02-08 18:05:17 -0500

    routes

--- routes/user.js
+++ routes/user.js
@@ -8,9 +8,9 @@ const { getCurrentUser, updateUserProfile } = require("../controllers/user");
 router.use(authMiddleware);
 
 // Route to get the logged-in user data
-router.get("/me", authMiddleware, getCurrentUser); // changed from /me ?
+router.get("/me", authMiddleware, getCurrentUser); // changed from /users/me ?
 
 // Route to update the user profile
-router.patch("/me", authMiddleware, updateUserProfile);
+router.patch("/me", authMiddleware, updateUserProfile); // changed from /users/me ?
 
 module.exports = router;
